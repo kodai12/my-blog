6df10040780217a8952821691610e903ea6b153e
kodai12 <sakochi.kodai1209@gmail.com>
2019-02-01 19:48:56 +0900

    add pages directory

---
+++ src/pages/Top.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Container, Section, Tile } from 'react-bulma-components/full';
+import AppHeader from '../components/Header.js';
+import Article from '../components/Article.js';
+
+class TopPage extends Component {
+  render() {
+    return (
+      <div className="my-blog">
+        <AppHeader />
+        <Section>
+          <Container>
+            <Tile kind="parent">
+              <Tile size={4} renderAs="article" kind="child">
+                <Article />
+              </Tile>
+            </Tile>
+          </Container>
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default TopPage;
