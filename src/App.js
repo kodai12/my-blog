d08b56ea7b2320317e544b5301ee7e9a30e106ac
kodai12 <sakochi.kodai1209@gmail.com>
2019-02-01 19:12:08 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Container, Section, Tile } from 'react-bulma-components/full';
+import AppHeader from './components/Header.js';
 import Article from './components/Article.js';
 
 class App extends Component {
   render() {
     return (
       <div className="my-blog">
+        <AppHeader />
         <Section>
           <Container>
             <Tile kind="parent">
