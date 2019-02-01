0b3b7abf25b7e57a1af5cc7b839c14b1cac29705
kodai12 <sakochi.kodai1209@gmail.com>
2019-02-01 22:58:06 +0900

    router 導入

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-import { Container, Section, Tile } from 'react-bulma-components/full';
-import AppHeader from './components/Header.js';
-import Article from './components/Article.js';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import TopPage from './pages/Top.js';
+import NotFoundPage from './pages/NotFound.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="my-blog">
-        <AppHeader />
-        <Section>
-          <Container>
-            <Tile kind="parent">
-              <Tile size={4} renderAs="article" kind="child">
-                <Article />
-              </Tile>
-            </Tile>
-          </Container>
-        </Section>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={TopPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Content, Heading } from 'react-bulma-components/full';
 
 class Article extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 1,
       title: 'sample',
       updateDate: '2019-2-19',
       description: 'testtesttes',
@@ -13,18 +15,20 @@ class Article extends Component {
 
   render() {
     return (
-      <Card className="article">
-        <Card.Header>
-          <Card.Header.Title>{this.state.title}</Card.Header.Title>
-        </Card.Header>
-        <Card.Content>
-          <Content>
-            <Heading subtitle size={3}>{this.state.description}</Heading>
-            <br />
-            <time>{this.state.updateDate}</time>
-          </Content>
-        </Card.Content>
-      </Card>
+      <Link to={`/article/${this.state.id}`}>
+        <Card className="article">
+          <Card.Header>
+            <Card.Header.Title>{this.state.title}</Card.Header.Title>
+          </Card.Header>
+          <Card.Content>
+            <Content>
+              <Heading subtitle size={3}>{this.state.description}</Heading>
+              <br />
+              <time>{this.state.updateDate}</time>
+            </Content>
+          </Card.Content>
+        </Card>
+      </Link>
     );
   }
 }
