b3676b6ebed0915564054b4afd1550fba0b7caa1
kodai12 <sakochi.kodai1209@gmail.com>
2019-02-02 22:43:50 +0900

    eslint導入

---
+++ .eslintrc.js
@@ -0,0 +1,92 @@
+module.exports = {
+  'parser': 'babel-eslint',
+  'extends': ['eslint:recommended', 'plugin:react/recommended'],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+  ],
+  'globals': {
+    "window": true,
+    "document": true,
+    "it": true,
+  },
+  'rules': {
+    'indent': 'off',
+    'indent-legacy': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline'
+    ],
+    'no-trailing-spaces': [
+      'error'
+    ],
+    'keyword-spacing': [
+      'error',
+      { 'before': true, 'after': true }
+    ],
+    'key-spacing': [
+      'error',
+      { 'mode': 'minimum' }
+    ],
+    'comma-spacing': [
+      'error',
+      { 'before': false, 'after': true }
+    ],
+    'space-before-blocks': [
+      'error'
+    ],
+    'arrow-spacing': [
+      'error',
+      { "before": true, "after": true }
+    ],
+    'space-in-parens': [
+      'error',
+      'never'
+    ],
+    'dot-notation': [
+      'error'
+    ],
+    'brace-style': [
+      'error',
+      '1tbs'
+    ],
+    'no-else-return': [
+      'error'
+    ],
+    'no-unused-vars': [
+      'warn',
+      { 'ignoreRestSiblings': true }
+    ],
+    'no-console': 'off',
+    'no-useless-escape': 'off',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react/prefer-stateless-function': [
+      2,
+      { 'ignorePureComponents': true },
+    ],
+    'react/no-string-refs': 'off', // TODO
+  }
+}
